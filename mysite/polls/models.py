9c7b87ecaf43b1996d108e0da27a60f58d633339
Ram81 <ramramrakhya81@gmail.com>
2016-12-20 20:41:32 +0530

    Database API Usage

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
-
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,8 +9,17 @@ class Question(models.Model):
 	question_text=models.CharField(max_length=200)
 	pub_date=models.DateTimeField('Publish Date')
 
+	def __str__(self):
+		return self.question_text
+
+	def was_published_recently(self):
+		return self.pub_date>=timezone.now()-datetime.timedelta(days=1)
+
 class choice(models.Model):
 	question=models.ForeignKey(Question,on_delete=models.CASCADE)
 	choice_text=models.CharField(max_length=200)
 	votes=models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.choice_text
 	
