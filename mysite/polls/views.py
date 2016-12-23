fac0c09525059422d83a22f25ff20391b8f2b0d6
Ram81 <ramramrakhya81@gmail.com>
2016-12-23 15:50:11 +0530

    Templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import Question
+from django.template import loader
 
 # Create your views here.
 def index(request):
 	list_question=Question.objects.order_by('-pub_date')[:5]
-	output=' ,'.join([q.question_text for q in list_question])
-	return HttpResponse(output)
+	template=loader.get_template('polls/index.html')
+	context={
+		'list_question':list_question,
+	}
+	return HttpResponse(template.render(context,request))
 
 def detail(request,question_id):
 	return HttpResponse("<h2> You are Looking at Question %s.</h2>"% question_id)
