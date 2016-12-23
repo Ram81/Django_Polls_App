ea682a3fbc52753efc1895085965a7716d2dbb63
Ram81 <ramramrakhya81@gmail.com>
2016-12-23 16:31:07 +0530

    Removing Hardcoded Urls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns=[
 	url(r'^$',views.index,name='index'),
+
+# modifying urls for index.html links to detail page to some other using {% url 'detail' l.id %}
+#	url(r'^specifics/(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+
 	url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
 	url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
 	url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
