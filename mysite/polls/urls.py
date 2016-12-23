0178defb003611e9613c13d1d0164eda9cc3e142
Ram81 <ramramrakhya81@gmail.com>
2016-12-23 16:36:43 +0530

    URL Namespaces

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls import url
 
 from . import views
 
+#using URL Namespaces
+app_name='polls'
+
 urlpatterns=[
 	url(r'^$',views.index,name='index'),
 
+# Removing hardoded urls
 # modifying urls for index.html links to detail page to some other using {% url 'detail' l.id %}
 #	url(r'^specifics/(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
 
