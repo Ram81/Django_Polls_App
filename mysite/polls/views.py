df0249b49a104888f7ee17194e3b10f79e1b07b7
Ram81 <ramramrakhya81@gmail.com>
2016-12-23 15:39:06 +0530

    Top 5 questions & links

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello Folks ! Welcome to first Django App.")
+	list_question=Question.objects.order_by('-pub_date')[:5]
+	output=' ,'.join([q.question_text for q in list_question])
+	return HttpResponse(output)
+
+def detail(request,question_id):
+	return HttpResponse("<h2> You are Looking at Question %s.</h2>"% question_id)
+
+def results(request,question_id):
+	message="You are Looking for %s."
+	return HttpResponse(message % question_id)
+
+def vote(request,question_id):
+	return HttpResponse("<h2> Votes for Question %s. </h2>"% question_id)
