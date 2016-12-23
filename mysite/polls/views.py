d49cb7259ec5bb88c9ceb18a5955d247234f9116
Ram81 <ramramrakhya81@gmail.com>
2016-12-23 16:11:44 +0530

    using get_object_or_404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404
 
 from .models import Question
@@ -15,11 +15,13 @@ def index(request):
 	return render(request,'polls/index.html',context)
 
 def detail(request,question_id):
-	try:
-		q=Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("Question You are looking for doesn't exists!")
-	return render(request,'polls/details.html',{ 'question':q })
+#	try:
+#		q=Question.objects.get(pk=question_id)
+#	except Question.DoesNotExist:
+#		raise Http404("Question You are looking for doesn't exists!")
+
+	q=get_object_or_404(Question,pk=question_id)
+	return render(request,'polls/detail.html',{ 'question':q })
 
 def results(request,question_id):
 	message="You are Looking for %s."
