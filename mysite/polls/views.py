2ddb5f9f8fcbe6f585cac2f87773d3d94e49084d
Ram81 <ramramrakhya81@gmail.com>
2016-12-24 21:52:19 +0530

    Test B Fixed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
+from django.utils import timezone
 from .models import Question,choice
 
 # Create your views here.
@@ -13,7 +14,7 @@ class IndexView(generic.ListView):
 	context_object_name='list_question'
 
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model=Question
