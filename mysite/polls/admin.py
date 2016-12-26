59a9c4ef128407eba6c2f32ea5697fc03b50261a
Ram81 <ramramrakhya81@gmail.com>
2016-12-26 19:32:05 +0530

    Admin Site Customization

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Question
+
 # Register your models here.
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+	fields=['pub_date','question_text']
+
+admin.site.register(Question,QuestionAdmin)
+
+
