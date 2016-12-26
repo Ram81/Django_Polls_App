73abcf0570b746c5b172ba262d8c52b8e192194b
Ram81 <ramramrakhya81@gmail.com>
2016-12-26 19:40:56 +0530

    Admin Site Customization

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question,choice
 
 # Register your models here.
+
+class ChoiceInline(admin.StackedInline):
+	model=choice
+	extra=3
 
 class QuestionAdmin(admin.ModelAdmin):
 	fieldsets=[
 		(None,{'fields' :['question_text']}),
-		('Date Information',{'fields':['pub_date']}),
+		('Date Information',{'fields':['pub_date'],'classes':['collapse']}),
 	]
+	inlines=[ChoiceInline]
 
 admin.site.register(Question,QuestionAdmin)
 
