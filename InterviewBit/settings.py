80b4beb26cce47dc55f6f9dca9b7c5e085b45bda
Ram81 <ramramrakhya81@gmail.com>
2018-01-11 19:53:16 +0530

    Add Support for Heroku Deployment

--- InterviewBit/settings.py
+++ InterviewBit/settings.py
@@ -114,6 +114,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Session expiration
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
